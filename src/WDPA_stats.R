b442994dd054a882066cbcd97bfd80863ca346a9
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-04-15 11:48:26 +0300

    Reorganize WDPA stats file

---
+++ src/WDPA_stats.R
@@ -0,0 +1,18 @@
+library('ProjectTemplate')
+load.project()
+
+source("config/config.R")
+
+wdpa_db <- src_postgres(dbname = DBNAME, host = HOST, port = PORT, user = USER,
+                        password = PASSWORD)
+
+wdpa <- tbl(wdpa_db, "wdpa_150312")
+
+pa_per_iso3 <- wdpa %>%
+  group_by(iso3, iucn_cat) %>%
+  summarise(
+    count = n(),
+    area_km = sum(rep_area)
+  ) %>%
+  arrange(iso3, iucn_cat) %>%
+  as.data.frame()
