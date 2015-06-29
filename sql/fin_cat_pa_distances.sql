8b019cf7bb6558e90f687383147a64e1fe90696a
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-06-29 09:08:27 +0300

    Tweak fin PA SQL

--- sql/fin_cat_pa_distances.sql
+++ sql/fin_cat_pa_distances.sql
@@ -4,9 +4,18 @@ WITH fin_cat_pa AS (
     WHERE iso3 = 'FIN' AND iucn_cat IN ('Ia')
  )
 SELECT f1.wdpaid AS from_ID, f2.wdpaid AS to_ID,
-  ST_Distance(f1.geom, f2.geom) / 1000 AS distance_km
+  ST_Distance(f1.geog, f2.geog) / 1000 AS distance_km
 FROM fin_cat_pa f1 CROSS JOIN fin_cat_pa f2
 WHERE f1.wdpaid < f2.wdpaid
 ORDER BY f1.wdpaid, f2.wdpaid
--- geometry type runs in 817 ms
--- geography type runs in 20864 ms
+-- When using geom (geometry type):
+--   geometry type runs in 817 ms
+--   geography (casting) type runs in 50282 ms
+-- When using geog (gegraphy type):
+--   geometry (casting) type runs in 978 ms
+--   geography type runs in 49608 ms
+-- AFTER CLUSTERING 
+--   CLUSTER wdpa.wdpa_poly USING wdpa_poly_geog_idx;
+-- When using geog (gegraphy type):
+--   geometry (casting) type runs in  ms
+--   geography type runs in 206853 ms 
