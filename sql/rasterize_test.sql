12326f22187da1b2cf07f17944e5c41d3bcb45f4
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-07-02 10:41:49 +0300

    Add rasterization test SQL code

---
+++ sql/rasterize_test.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE extracttoraster_finlandIa AS
+SELECT ST_ExtractToRaster(
+         ST_AddBand(ST_MakeEmptyRaster(rast_finlandIa), '32BF'::text, -9999, -9999), 
+         'wdpa', 
+         'wdpa_poly_geom', 
+         'geom', 
+         'wdpaid', 
+         'VALUE_OF_BIGGEST') rast_finlandIa
+FROM (SELECT * FROM wdpa.wdpa_poly
+      WHERE iso3 = 'FIN' AND iucn_cat IN ('Ia')) AS finlandIa;
