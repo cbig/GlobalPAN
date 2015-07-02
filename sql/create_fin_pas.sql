1467d91f866a3386833f61ae3dfcd5d51c260911
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-07-02 10:41:35 +0300

    Add SQL for separating only Finland from the WDPA

---
+++ sql/create_fin_pas.sql
@@ -0,0 +1,10 @@
+﻿SELECT *
+INTO wdpa.wdpa_poly_fin
+FROM wdpa.wdpa_poly
+WHERE iso3 = 'FIN';
+
+-- Define a primary key
+ALTER TABLE wdpa.wdpa_poly_fin ADD PRIMARY KEY (gid);
+
+-- Spatially enable it
+SELECT Populate_Geometry_Columns('wdpa.wdpa_poly_fin'::regclass);
