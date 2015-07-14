49ea26ecedbc970f972701992459c921edf08ec6
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-07-14 11:37:30 +0300

    SQL for creating PA extent table

---
+++ sql/PA_extents.sql
@@ -0,0 +1,4 @@
+﻿SELECT wdpaid, iso3, SUM(rep_area) as area, ST_YMin(ST_Extent(geom)) as y_min, ST_XMin(ST_Extent(geom)) as x_min, ST_YMax(ST_Extent(geom)) as y_max, ST_XMax(ST_Extent(geom)) as x_max INTO wdpa.wdpa_pa_extents
+FROM wdpa.wdpa_poly_geom 
+GROUP BY wdpaid, iso3
+ORDER BY wdpaid, iso3;
