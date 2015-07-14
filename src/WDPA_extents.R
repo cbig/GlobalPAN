768fcc12e0ba220e91771a6df1ec5b6c33fa2929
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-07-14 13:08:20 +0300

    Add plot for per latitude area of PAs

--- src/WDPA_extents.R
+++ src/WDPA_extents.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(ggplot2)
 library('ProjectTemplate')
-
+library(tidyr)
 
 load.project()
 options(scipen = 999)
@@ -14,16 +14,22 @@ wdpa <- tbl(wdpa_db, "wdpa_pa_extents")
 
 # Calculate extent y centroid and plot that in a histogram
 plot_data <- wdpa %>% 
-  mutate(y_centroid = (y_min + y_max) / 2) %>% 
-  as.data.frame(n = -1)
+  mutate(y_centroid = (y_min + y_max) / 2) %>%
+  as.data.frame(n = -1) %>% 
+  mutate(lat_group = cut(y_centroid, breaks = seq(-90, 90, 10))) 
   
 p1 <- ggplot(plot_data, aes(x = y_centroid)) + geom_histogram(binwidth = 10) + 
   coord_flip() + xlim(c(-90, 90)) + scale_x_discrete(limits = seq(-90, 90, 10)) + 
   xlab("Latitude\n")
 
 # Create latitude group manually
-plot_data$lat_group <- cut(plot_data$y_centroid, breaks = seq(-90, 90, 10))
+plot_grouped_data <- plot_data %>% 
+  group_by(lat_group) %>% 
+  summarise(
+    area = sum(area)
+  ) %>% 
 
-#p2 <- ggplot(plot_data, aes(x = area, y = y_centroid, group = lat_group)) + 
-#  geom_bar(stat = "identity") + coord_flip() + xlim(c(-90, 90)) + 
-#  scale_x_discrete(limits = seq(-90, 90, 10)) + xlab("Latitude\n")
+p2 <- ggplot(plot_grouped_data, aes(x = lat_group, y = area)) + 
+  geom_bar(stat = "identity") + coord_flip() + xlab("Latitude\n") + 
+  ylab(expression("\nArea (km2)"))
+
