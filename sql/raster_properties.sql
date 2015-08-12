75327bbb63eb2e335c4959de7e6bd920cf54467a
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-08-12 12:40:17 +0300

    Add old sql files

---
+++ sql/raster_properties.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+    rid AS r, filename,
+    ST_Width(rast) AS w,
+    ST_Height(rast) AS h,
+    round(ST_PixelWidth(rast)::numeric,4) AS pw,
+    round(ST_PixelHeight(rast)::numeric,4) AS ph,
+    ST_SRID(rast) AS srid,
+    ST_BandPixelType(rast,1) AS bt
+FROM wdpa_raster.cell_area
--- sql/rasterize_test.sql
+++ sql/rasterize_test.sql
@@ -1,10 +1,9 @@
 ﻿CREATE TABLE extracttoraster_finlandIa AS
 SELECT ST_ExtractToRaster(
-         ST_AddBand(ST_MakeEmptyRaster(rast_finlandIa), '32BF'::text, -9999, -9999), 
+         ST_AddBand(ST_MakeEmptyRaster(rast), '32BF'::text, -9999, -9999), 
          'wdpa', 
-         'wdpa_poly_geom', 
+         'wdpa_poly_geom_fin', 
          'geom', 
          'wdpaid', 
-         'VALUE_OF_BIGGEST') rast_finlandIa
-FROM (SELECT * FROM wdpa.wdpa_poly
-      WHERE iso3 = 'FIN' AND iucn_cat IN ('Ia')) AS finlandIa;
+         'VALUE_OF_BIGGEST') rast
+FROM (SELECT rast FROM wdpa_raster.cell_area) AS rast;
